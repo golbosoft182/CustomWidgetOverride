2c6ca63e62df2f35166ca272947a5e2c3c36a1af
eka <ekahandik@gmail.com>
2022-06-13 22:55:48 +0700

    added github

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @category    Lingble
+ * @package     CustomWidgetOverride
+ *
+ *
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Lingble_CustomWidgetOverride',
+    __DIR__
+);
